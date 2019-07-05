f92e88403c32a4a2becf532a2690e216f6b70fd4
Jonas <jturbeaux@pm.me>
2019-07-05 12:00:01 +0400

    maj

--- DjangoFiles/launch.sh
+++ DjangoFiles/launch.sh
@@ -1,3 +1,3 @@
 cd /DjangoFiles
-gunicorn CashlessOi.wsgi --log-level=debug --log-file /DjangoFiles/www/gunicorn.logs -w 5 -b 0.0.0.0:8000
+gunicorn Cashless.wsgi --log-level=debug --log-file /DjangoFiles/www/gunicorn.logs -w 5 -b 0.0.0.0:8000
 
